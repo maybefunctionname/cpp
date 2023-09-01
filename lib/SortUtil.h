62288386dc7415ca0f14e0453850c68e13655de8
maybefunctionname <526466136@qq.com>
2023-09-02 00:14:15 +0800

    补充插入、选择排序

--- lib/SortUtil.h
+++ lib/SortUtil.h
@@ -21,6 +21,14 @@ public:
 	void sort(int* arr, int len) {
 		if (this->kind == SORT_KIND::BUBBLE){
 			this->bubbleSort(arr, len);
+		} 
+		else if (this->kind == SORT_KIND::SELECT)
+		{
+			this->selectSort(arr, len);
+		}
+		else if (this->kind == SORT_KIND::INSERT)
+		{
+			this->insertSort(arr, len);
 		}
 	}
 private:
@@ -38,4 +46,47 @@ private:
 			}
 		}
 	}	
+	void selectSort(int* arr, int len)
+	{
+		int left = 0;
+		int right = len - 1;
+		while (left < right)
+		{
+			int minIndex = left;
+			int maxIndex = right;
+			for (int i = left; i <= right; i++)
+			{
+				if (arr[minIndex] > arr[i])
+					minIndex = i;
+				if (arr[maxIndex] < arr[i])
+					maxIndex = i;
+			}
+			if (minIndex != left)
+				BaseUtil::swap(arr[minIndex], arr[left]);
+			if (maxIndex == left)
+				maxIndex = minIndex;
+			if (maxIndex != right)
+				BaseUtil::swap(arr[maxIndex], arr[right]);
+			left++;
+			right--;
+		}	
+	}
+	void insertSort(int* arr, int len)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (i > 0 && arr[i-1] > arr[i])
+			{
+				int j = i - 1;
+				int temp = arr[i];
+				while (j >= 0 && arr[j] > temp)
+				{
+					arr[j+1] = arr[j];
+				   	j--;
+				}
+				arr[j+1] = temp;
+			}
+		}
+	}
+
 };
